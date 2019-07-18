b9da19f429868a8849b72da0852f8db516e0d238
Shawn Liu <shawn.jw.liu@gmail.com>
2019-07-18 17:53:28 +0800

    Update flannel.sh

--- flannel.sh
+++ flannel.sh
@@ -3,6 +3,7 @@ images=(
     coreos/flannel:v0.11.0-arm
     coreos/flannel:v0.11.0-ppc64le
     coreos/flannel:v0.11.0-s390x
+    kubernetes-ingress-controller/nginx-ingress-controller:0.24.1
 )
 
 for imageName in ${images[@]} ; do
