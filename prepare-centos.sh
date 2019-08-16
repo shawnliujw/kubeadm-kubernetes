1648fd0e9f7c43046745e5bfaca7d8c606385357
Shawn Liu <shawn.jw.liu@gmail.com>
2019-08-16 10:13:07 +0800

    Update prepare-centos.sh

--- prepare-centos.sh
+++ prepare-centos.sh
@@ -4,6 +4,8 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum install -y docker-ce
 
+systemctl enable docker.service
+
 
 yum install -y ebtables ethtool
 
